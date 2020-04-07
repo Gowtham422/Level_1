f3190fff836585ef5bed3f7b18b103f3d1ca58d7
Gowtham422 <63245218+Gowtham422@users.noreply.github.com>
2020-04-07 11:37:38 +0530

    Add files via upload

---
+++ Whileloop.java
@@ -0,0 +1,19 @@
+package pkg_2;
+
+public class Class_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         
+		int a = 100;
+		
+		while (a > 20) {
+			a-=5;
+			if (a==75) {
+				continue;
+			}
+			System.out.println(a);
+		}
+	}
+
+}
